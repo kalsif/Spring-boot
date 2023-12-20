6e6d1f22f0477455b90bf67c15b299a3666c4999
Antonio <antoniodat93@gmail.com>
2023-12-20 10:43:13 +0100

    Spring

---
+++ demo-clean/demo-clean/src/main/java/com/example/democlean/DemoCleanApplication.java
@@ -0,0 +1,13 @@
+package com.example.democlean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCleanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCleanApplication.class, args);
+	}
+
+}
