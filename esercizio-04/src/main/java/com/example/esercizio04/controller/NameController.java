213805d56b4c36b08d3c8f9f8ce9c96583c2e93a
Antonio <antoniodat93@gmail.com>
2024-01-11 10:11:10 +0100

    esercizio 04

---
+++ esercizio-04/src/main/java/com/example/esercizio04/controller/NameController.java
@@ -0,0 +1,39 @@
+package com.example.esercizio04.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1")
+public class NameController {
+
+    @GetMapping(path = "")
+    @Operation(summary = "name", description = "The name of user")
+    @ResponseStatus(HttpStatus.OK)
+    @ApiResponses({
+            @ApiResponse(responseCode = "400" , description = "Bad input"),
+            @ApiResponse(responseCode = "500", description = "Internal error"),
+            @ApiResponse(responseCode = "202", description = "created")
+    })
+    public String ciao(@RequestParam String name){
+        return "Ciao " + name ;
+    }
+
+
+    @PostMapping("/reverse")
+    @Operation(summary = "reverse-name", description = "The reverse name of the user")
+    @ResponseStatus(HttpStatus.OK)
+    @ApiResponses({
+            @ApiResponse(responseCode = "202", description = "Created"),
+            @ApiResponse(responseCode = "400" , description = "Bad input")
+    })
+    public StringBuilder reverse(String name){
+        StringBuilder reverse = new StringBuilder(name);
+        reverse.reverse();
+        return reverse;
+    }
+}
