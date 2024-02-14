bfe7239e47b41f3c59d3227fe7577f43ccd2f5f7
Antonio <antoniodat93@gmail.com>
2024-02-14 13:00:02 +0100

    esercizio 20

---
+++ esercizio-20/src/main/java/com/example/esercizio20/Esercizio20Application.java
@@ -0,0 +1,13 @@
+package com.example.esercizio20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Esercizio20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Esercizio20Application.class, args);
+	}
+
+}
---
+++ esercizio-20/src/test/java/com/example/esercizio20/Esercizio20ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.esercizio20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Esercizio20ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
